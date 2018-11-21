3bee910b9bb1f57f9a67a75af98aecbc03a40044
Izabela Kuzniar <iza8848@gmail.com>
2018-11-21 13:45:09 +0100

    no message

--- src/js/LogInPage.ts
+++ src/js/LogInPage.ts
@@ -17,25 +17,28 @@ interface IUser {
 
 let usernameInput: HTMLInputElement = <HTMLInputElement>document.getElementById("username");
 let passwordInput: HTMLInputElement = <HTMLInputElement>document.getElementById("password");
+let selectType: HTMLSelectElement = <HTMLSelectElement> document.getElementById("selectType");
+
 let loginButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("loginButton");
 loginButton.addEventListener("click", login);
 
 function login(): void {
 
-    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/login/" + usernameInput.value + "/" + passwordInput.value;
+    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/login/" + usernameInput.value + "/" + passwordInput.value + "/" + selectType.value;
     axios.get(uri)
         .then(function (response: AxiosResponse): void {
             console.log(response.data);
-            if (response.data === true) {
+            if (response.data === true)
+                if(selectType.value === "U")
                 window.location.href = 'http://localhost:3000/index.htm';
-            }
-            else {
-                alert("Wrong credentials")
-            }
-        })
-        .catch(function (error: AxiosError): void {
-            console.log(error);
+                else
+                window.location.href = 'http://localhost:3000/EnvironmentPage.html';
+            if (response.data === false)
             alert("Wrong credentials")
-        }
+        })
+        .catch (function (error: AxiosError): void {
+    console.log(error);
+    alert("Wrong credentials")
+}
         )
 }
