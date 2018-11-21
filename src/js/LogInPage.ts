ac4f3f4209c1f8161421901cb66f6759eab933b3
Izabela Kuzniar <iza8848@gmail.com>
2018-11-21 18:15:53 +0100

    tries of login in ts

--- src/js/LogInPage.ts
+++ src/js/LogInPage.ts
@@ -42,3 +42,32 @@ function login(): void {
 }
         )
 }
+let users: Array<IUser> = [];
+
+function login2(): void{
+    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users";
+    axios.get<IUser[]>(uri)
+    .then(function(response: AxiosResponse<IUser[]>): void{
+        response.data.forEach((user: IUser) => {
+            if (user != null)
+            users.push(user);
+            
+                    });
+                    
+    })
+    .catch(function(error: AxiosError): void{
+        console.log(error);
+        alert("Wrong credentials")
+    })
+
+    users.forEach(element => {
+        if(usernameInput.value === element.username && usernameInput.value === element.password && selectType.value === element.typeOfUser){
+            if(element.typeOfUser === "U")
+            window.location.href = 'http://localhost:3000/index.htm';
+            if(element.typeOfUser === "S")
+            window.location.href = 'http://localhost:3000/EnvironmentPage.html';
+        }               
+        else alert("Wrong credentials")
+
+    })
+}
