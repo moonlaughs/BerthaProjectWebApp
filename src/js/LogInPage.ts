99ad4397764e3868a05bab8b7760de7e249a129c
Izabela Kuzniar <iza8848@gmail.com>
2018-11-23 14:05:07 +0100

    login changes

--- src/js/LogInPage.ts
+++ src/js/LogInPage.ts
@@ -24,16 +24,20 @@ loginButton.addEventListener("click", login);
 
 function login(): void {
 
-    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/login/" + usernameInput.value + "/" + passwordInput.value + "/" + selectType.value;
+    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/login/" + usernameInput.value + "/" + passwordInput.value;
     axios.get(uri)
         .then(function (response: AxiosResponse): void {
             console.log(response.data);
-            if (response.data === true)
-                if(selectType.value === "U")
-                window.location.href = 'http://localhost:3000/index.htm';
+            if (response.data !== null)
+            {
+                if (response.data.typeOfUser === "U")
+                    window.location.href = 'http://localhost:3000/index.htm';
+                else if (response.data.typeOfUser === "S")
+                    window.location.href = 'http://localhost:3000/EnvironmentPage.html';
                 else
-                window.location.href = 'http://localhost:3000/EnvironmentPage.html';
-            if (response.data === false)
+                alert("Wrong credentials")
+            }
+            if (response.data === null)
             alert("Wrong credentials")
         })
         .catch (function (error: AxiosError): void {
@@ -41,33 +45,4 @@ function login(): void {
     alert("Wrong credentials")
 }
         )
-}
-let users: Array<IUser> = [];
-
-function login2(): void{
-    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users";
-    axios.get<IUser[]>(uri)
-    .then(function(response: AxiosResponse<IUser[]>): void{
-        response.data.forEach((user: IUser) => {
-            if (user != null)
-            users.push(user);
-            
-                    });
-                    
-    })
-    .catch(function(error: AxiosError): void{
-        console.log(error);
-        alert("Wrong credentials")
-    })
-
-    users.forEach(element => {
-        if(usernameInput.value === element.username && usernameInput.value === element.password && selectType.value === element.typeOfUser){
-            if(element.typeOfUser === "U")
-            window.location.href = 'http://localhost:3000/index.htm';
-            if(element.typeOfUser === "S")
-            window.location.href = 'http://localhost:3000/EnvironmentPage.html';
-        }               
-        else alert("Wrong credentials")
-
-    })
 }
