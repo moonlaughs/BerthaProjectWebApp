bd96cd3d62000d2f2daf618c0357f003087372b8
Diiiya <33103806+Diiiya@users.noreply.github.com>
2018-11-29 15:07:19 +0200

    Finished with the ts for the Scietist View on pages index, Chart & EnvUs

--- src/js/index.ts
+++ src/js/index.ts
@@ -41,42 +41,15 @@ interface IEnvironment{
     dateTimeInfo: Date;
 }
 
-let output: HTMLDivElement = <HTMLDivElement>document.getElementById("output");
-let showallUsersButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("showAllUsersButton");
-showallUsersButton.addEventListener("click", showAllUsers);
-
-function showAllUsers(): void {
-    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users";
-    axios.get<IUser[]>(uri)
-        .then(function (response: AxiosResponse<IUser[]>): void {
-            let result: string = "<ol>";
-            response.data.forEach((user: IUser) => {
-                result += "<li>" + user.firstName + " " + user.lastName + `<button id='goToChart${user.id}'>Details</button>` + "</li>"
-
-            });
-            result += "</ol>";
-            output.innerHTML = result;
-            response.data.forEach((user: IUser) => {
-                let gotoChartButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById(`goToChart${user.id}`);
-                gotoChartButton.addEventListener("click", function () {
-                    // pass in `this` (the element), and someOtherVar
-                    goToChart(user.id);
-                });
-            });
-        })
-        .catch(function (error: AxiosError): void {
-            if (error.response) {
-                output.innerHTML = error;
-            }
-            else { output.innerHTML = error; }
-        });
-
-}
-
 let showSelectedUser: HTMLButtonElement = <HTMLButtonElement>document.getElementById("showSelectedUser");
 showSelectedUser.addEventListener("click", showUser);
 
-function goToChart(id: Number): void {
+function goToEnvChartAndTable(id: Number): void {
+
+    window.location.href = 'EnvironmentUser.html?id=' + id;
+}
+
+function goToHealthChartAndTable(id: Number): void {
 
     window.location.href = 'Chart.html?id=' + id;
 }
@@ -90,13 +63,17 @@ function showUser(): void {
     axios.get<IUser>(uri)
         .then(function (response: AxiosResponse<IUser>): void {
             console.log(response.data);
-            let result: string = response.data.firstName + response.data.lastName + `<button id='goToChart${response.data.id}'>Details</button>`;
+            let result: string = response.data.firstName + " " + response.data.lastName + " " + `<button id='goToHealthChartAndTable${response.data.id}'>Check Health Data</button>` + " " + `<button id='goToEnvChartAndTable${response.data.id}'>Check ENV Data</button>`;
             selOutput.innerHTML = result;
-              let gotoChartButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById(`goToChart${response.data.id}`);
-              gotoChartButton.addEventListener("click", function () {
+              let gotoHealthChartAndTableButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById(`goToHealthChartAndTable${response.data.id}`);
+              gotoHealthChartAndTableButton.addEventListener("click", function () {
+                    goToHealthChartAndTable(response.data.id);      
+              });
+              let gotoEnvChartAndTableButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById(`goToEnvChartAndTable${response.data.id}`);
+              gotoEnvChartAndTableButton.addEventListener("click", function () {
                     // pass in `this` (the element), and someOtherVar
-                goToChart(response.data.id);
-            });
+                    goToEnvChartAndTable(response.data.id);
+              });
         })
         .catch(function (error: AxiosError): void {
             if (error.response) {
