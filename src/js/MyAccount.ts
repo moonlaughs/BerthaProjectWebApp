76bea4a56e7e9ef4c633b9eb5693675db8134bb8
Milena Ognyanova <mile0416@edu.easj.dk>
2018-11-29 10:50:48 +0100

    Trying to fix many errors

--- src/js/MyAccount.ts
+++ src/js/MyAccount.ts
@@ -19,16 +19,16 @@ let infoButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("
 infoButton.addEventListener("click", AccountInfo)
 
 function AccountInfo(): void {
-    var regex = /[?&]([^=#]+)=([^&#]*)/g,
+    /*var regex = /[?&]([^=#]+)=([^&#]*)/g,
         url = window.location.href;
     var params: any = {},
         match;
     while (match = regex.exec(url)) {
         params[match[1]] = match[2];
-    }
-    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users" + params.id;
+    }*/
+    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/1"; //+ //params.id;
     axios.get<IUser>(uri)
-        .then(function (response: AxiosResponse<IUser[]>): void {
+        .then(function (response: AxiosResponse<IUser>): void {
             let result: string = "<table></th><th>First Name</th><th>Last Name</th><th>Username</th><th>Age</th><th>Gender</th>"
             response.data.forEach((user: IUser) => {
                 result += "<tr><td>" + user.firstName + "</td><td>" + user.lastName + "</td><td>" + user.userName + "</td><td>" + user.age + "</td><td>" + user.gender + "</td></tr>"
