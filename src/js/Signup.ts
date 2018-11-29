1246b22e6ee9c531831d95899a986a5c5ede4814
Meg <megi0005@edu.easj.dk>
2018-11-29 11:07:25 +0100

    fixed log in and sign up page. tried to create a pop up sign up page, it needs a bit of fixing

--- src/js/Signup.ts
+++ src/js/Signup.ts
@@ -9,6 +9,7 @@ interface IUser {
     lastName: string;
     userName: string;
     pass: string;
+    pass2:string;
     age: number;
     gender: string;
     typeOfUser: string;
@@ -16,7 +17,7 @@ interface IUser {
 
 let firstName: HTMLInputElement = <HTMLInputElement>document.getElementById("firstName");
 let lastName: HTMLInputElement = <HTMLInputElement>document.getElementById("lastName");
-let username: HTMLInputElement = <HTMLInputElement>document.getElementById("username");
+let userName: HTMLInputElement = <HTMLInputElement>document.getElementById("userName");
 let pass: HTMLInputElement = <HTMLInputElement>document.getElementById("pass");
 let pass2: HTMLInputElement = <HTMLInputElement>document.getElementById("pass2");
 let age: HTMLInputElement = <HTMLInputElement>document.getElementById("age");
@@ -25,13 +26,10 @@ let selectGender: HTMLSelectElement = <HTMLSelectElement>document.getElementById
 let addButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("addButton");
 addButton.addEventListener("click", AddUser);
 
-let backButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("back");
-backButton.addEventListener("click", () => window.location.href = 'LoginPage.html')
-
 function AddUser(): void {
     let myfirstname: string = firstName.value;
     let mylastname: string = lastName.value;
-    let myusername: string = username.value;
+    let myusername: string = userName.value;
     let mypass: string = pass.value;
     let myage: number = parseInt(age.value);
     let mygender: string = selectGender.value;
@@ -42,7 +40,7 @@ function AddUser(): void {
         axios.post<IUser>(uri, { firstName: myfirstname, lastName: mylastname, userName: myusername, pass: mypass, age: myage, gender: mygender, typeOfUser: myTypeOfUser })
             .then(function (response: AxiosResponse) {
                 console.log(response.status + " " + response.statusText);
-                alert("User " + username + " was succesfully added");
+                alert("User " + userName + " was succesfully added");
                 window.location.href = 'LoginPage.html';
             })
             .catch((error: AxiosError) => {
