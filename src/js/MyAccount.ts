d0da1654442dfe3f777aad06e89c90e13dd08ec9
Izabela Kuzniar <iza8848@gmail.com>
2018-11-29 10:57:19 +0100

    some changes

--- src/js/MyAccount.ts
+++ src/js/MyAccount.ts
@@ -26,9 +26,9 @@ function AccountInfo(): void {
     while (match = regex.exec(url)) {
         params[match[1]] = match[2];
     }
-    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users" + params.id;
+    let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/1"; //+ "?id="+ "1";//params.id;
     axios.get<IUser>(uri)
-        .then(function (response: AxiosResponse<IUser[]>): void {
+        .then(function (response: AxiosResponse<IUser>): void {
             let result: string = "<table></th><th>First Name</th><th>Last Name</th><th>Username</th><th>Age</th><th>Gender</th>"
             response.data.forEach((user: IUser) => {
                 result += "<tr><td>" + user.firstName + "</td><td>" + user.lastName + "</td><td>" + user.userName + "</td><td>" + user.age + "</td><td>" + user.gender + "</td></tr>"
