14040ad794188303dc2b5fe175cadc2ef3d4c65b
Izabela Kuzniar <iza8848@gmail.com>
2018-11-29 11:22:55 +0100

    changes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,18 +10,18 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 module.exports = {
   // which files should webpack watch and transpile
   entry: {
-    LogInPage: ['./src/LogInPage.html', './src/js/LogInPage.ts'],
-    index: ['./src/index.htm', './src/js/index.ts'],
-    EnvironmentPage: ['./src/EnvironmentPage.html', './src/js/EnvironmentPage.ts'],
-    Signup: ['./src/SignupPage.html', './src/js/Signup.ts'],
-    Homepage: ['./src/Homepage.html', './src/js/Homepage.ts'],
-    Chart: ['./src/Chart.html', './src/js/Chart.ts'],
-    HomeUser: ['./src/HomeUser.html', './src/js/HomeUser.ts'],
-    HealthUser: ['./src/HealthUser.html', './src/js/HealthUser.ts'],
-    EnvironmentUser: ['./src/EnvironmentUser.html', './src/js/EnvironmentUser.ts'],
-    Temp: ['./src/Temp.html', './src/js/Temp.ts'],
-    AboutUs: ['./src/AboutUs.html'],
-    MyAccount: ['./src/MyAccount.html', './src/js/MyAccount.ts']
+    LogInPage: ['./src/LogInPage.html', './src/scss/styles.scss','./src/js/LogInPage.ts'],
+    index: ['./src/index.htm', './src/scss/styles.scss', './src/js/index.ts'],
+    EnvironmentPage: ['./src/EnvironmentPage.html', './src/scss/styles.scss', './src/js/EnvironmentPage.ts'],
+    Signup: ['./src/SignupPage.html', './src/scss/styles.scss', './src/js/Signup.ts'],
+    Homepage: ['./src/Homepage.html', './src/scss/styles.scss', './src/js/Homepage.ts'],
+    Chart: ['./src/Chart.html', './src/scss/styles.scss', './src/js/Chart.ts'],
+    HomeUser: ['./src/HomeUser.html', './src/scss/styles.scss', './src/js/HomeUser.ts'],
+    HealthUser: ['./src/HealthUser.html', './src/scss/styles.scss', './src/js/HealthUser.ts'],
+    EnvironmentUser: ['./src/EnvironmentUser.html', './src/scss/styles.scss', './src/js/EnvironmentUser.ts'],
+    Temp: ['./src/Temp.html', './src/scss/styles.scss', './src/js/Temp.ts'],
+    AboutUs: ['./src/AboutUs.html', './src/scss/styles.scss'],
+    MyAccount: ['./src/MyAccount.html', './src/scss/styles.scss', './src/js/MyAccount.ts']
   },
   module: {
     // rules webpack should follow when watching...
