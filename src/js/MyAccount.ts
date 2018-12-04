5bf2940c41d0d390c5c17c884b4b862c529e128f
Izabela Kuzniar <iza8848@gmail.com>
2018-12-04 10:56:05 +0100

    pushed sth

--- src/js/MyAccount.ts
+++ src/js/MyAccount.ts
@@ -23,7 +23,7 @@ let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net
 axios.get<IUser>(uri)
     .then(function (response: AxiosResponse<IUser>): void {
         console.log(response.data);
-        let result: string = "<table><tr><td>First Name</td><td>" + response.data.firstName + "</td></tr><tr><td>Last Name</td><td>" + response.data.lastName + "</td><tr><td>Username</td><td>" + response.data.userName + "</td><tr><td>Age</td><td>" + response.data.age + "</td><tr><td>Gender</td><td>" + response.data.gender + "</td></table>";
+        let result: string = "<table><tr><th>First Name</th><td>" + response.data.firstName + "</td></tr><tr><th>Last Name</th><td>" + response.data.lastName + "</td><tr><th>Username</th><td>" + response.data.userName + "</td><tr><th>Age</th><td>" + response.data.age + "</td><tr><th>Gender</th><td>" + response.data.gender + "</td></table>";
         Info.innerHTML = result;
     })
     .catch(function (error: AxiosError): void {
@@ -45,16 +45,32 @@ function updateCredentials(): void {
     let myusername: string = username.value;
     let mypassword: string = pass1.value;
 
+    
+
     if (pass1.value === pass2.value) {
-        let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/" + itemID;
-        axios.put<IUser>(uri, { userName: myusername, pass: mypassword })
-            .then((response: AxiosResponse) => {
-                console.log(response.status)
-                alert("Credentials were changed successfuly");
-            })
-            .catch(function (error: AxiosError): void {
-                console.log(error);
-            })
+        let uri1: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/" + itemID;
+
+    axios.get<IUser>(uri1)
+        .then(function (response: AxiosResponse<IUser>): void {
+            console.log(response.data);
+            var user: IUser = response.data;
+            let uri2: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/users/" + itemID;
+            axios.put<IUser>(uri2, { firstName: user.firstName, lastName: user.lastName, userName: myusername, pass: mypassword, age: user.age, gender: user.gender, typeOfUser: user.typeOfUser })
+                .then((response: AxiosResponse) => {
+                    console.log(response.status)
+                    alert("Credentials were changed successfuly");
+                })
+                .catch(function (error: AxiosError): void {
+                    console.log(error);
+                    alert("Something went wrong...");
+                }) 
+        })
+        .catch(function (error: AxiosError): void {
+            console.log(error);
+            alert("Something went wrong...");
+        });
+
+        
     }
     else alert("Password does not match!")
 }
--- src/js/Temp.ts
+++ src/js/Temp.ts
@@ -1,99 +1,79 @@
-import axios,{
-    AxiosResponse,
-    AxiosError
+import axios, {
+  AxiosResponse,
+  AxiosError
 } from "../../node_modules/axios";
 
-interface ITemp{
-    id : number;
-    Temp: string;   //number or string?????
-    DT : Date;
+interface ITemp {
+  DT: Date;
+  id: number;
+  Temp: number;
 }
 
-google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.load('current', { packages: ['corechart', 'line'] });
 google.charts.setOnLoadCallback(tempDataChart);
 
 function tempDataChart(): void {
-    /*var regex = /[?&]([^=#]+)=([^&#]*)/g,
-    url = window.location.href;
-    var params: any = {},
-    match;
-    while(match = regex.exec(url)) {
-        params[match[1]] = match[2];
-    }*/
+  let uri: string = "https://restservicefrombroadcast20181120110311.azurewebsites.net/api/dts";
 
-    let uri: string = "https://restservicefrombroadcast20181120110311.azurewebsites.net/api/dts";
-
-    axios.get<ITemp>(uri)
+  axios.get<ITemp>(uri)
     .then(function (response: AxiosResponse<ITemp[]>): void {
-        console.log(response);
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'X');
-        //data.addColumn('nmber', 'Date');
-        /*data.addColumn('string', 'X');
-        data.addColumn('number', 'Oxygen');
-        data.addColumn('number', 'Co2');
-        data.addColumn('number', 'Co');
-        data.addColumn('number', 'Pm25');
-        data.addColumn('number', 'Pm10');
-        data.addColumn('number', 'Ozon');
-        data.addColumn('number', 'Dust particles');
-        data.addColumn('number', 'Nitrogen Dioxide');
-        data.addColumn('number', 'Sulphur dioxide');*/
-        
+      console.log(response);
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'X');
+      data.addColumn('number', 'Temp');
 
+      response.data.sort((a: ITemp, b: ITemp) => {
+        return new Date(Date.parse(a.DT.toString())).getTime() - new Date(Date.parse(b.DT.toString())).getTime();
+      });
 
-        response.data.sort((a: ITemp, b: ITemp) => {
-            return new Date(Date.parse(a.DT.toString())).getTime() - new Date(Date.parse(b.DT.toString())).getTime();
-        });
-        var newarr = [response.data[0]];
-        for (var i=1; i<response.data.length; i++) {
-           if (response.data[i].DT.toString().split('T')[0]!=response.data[i-1].DT.toString().split('T')[0]){
-            newarr.push(response.data[i]);
-           }           
+      var newarr = [response.data[0]];
+      for (var i = 1; i < response.data.length; i++) {
+        if (response.data[i].dateTimeInfo.toString().split('T')[0] != response.data[i - 1].dateTimeInfo.toString().split('T')[0]) {
+          newarr.push(response.data[i]);
         }
+      }
 
+      newarr.forEach((temp: ITemp) => {
+        data.addRows([
+            [temp.DT.toString().split('T')[0], temp.Temp]
+          ]);
         
-
-        newarr.forEach((temp: ITemp) => {
-            data.addRows([
-                [parseInt(temp.Temp.split('-')[0])]
-              ]);
-            
-        });   
-  
-        var options = {
-          hAxis: {
-            title: 'Date',
-            titleColor: 'white',
-            textStyle: {
-              color: 'white',
-              fontSize: 16,
-              italic: true,
-              bold: true
-            }
-          },
-          vAxis: {
-            title: 'Temperature', 
-            titleColor: 'white',
-            textStyle: {
-              color: 'white',
-              fontSize: 16,
-              italic: true, 
-              bold: true
-            }
-          },
-          titleTextStyle: {
-            color: 'white',
-            fontSize: 16,
-            italic: true,
-            bold: true
-          },
-          backgroundColor: 'transparent', 
-          curveLine: 'none',
-          lineWidth: 3,
-          legendTextStyle: {color: 'white', italic: true}
+          var options = {
+            hAxis: {
+                title: 'Date',
+                titleColor: 'white',
+                textStyle: {
+                  color: 'white',
+                  fontSize: 16,
+                  italic: true,
+                  bold: true
+                }
+              },
+              vAxis: {
+                title: 'Health', 
+                titleColor: 'white',
+                textStyle: {
+                  color: 'white',
+                  fontSize: 16,
+                  italic: true, 
+                  bold: true
+                }
+              },
+              titleTextStyle: {
+                color: 'white',
+                fontSize: 16,
+                italic: true,
+                bold: true
+              },
+              backgroundColor: 'transparent', 
+              curveType: 'none',
+              lineWidth: 3,
+              dataColor: 'white',
+              legendTextStyle: {color: 'white', italic: true}
         };
-        var chart = new google.visualization.LineChart(document.getElementById('tempChart'));
-        chart.draw(data, options);
+  
+        var chart = new google.visualization.LineChart(document.getElementById('curve-Chart'));
+            chart.draw(data, options);
+    }); 
     })
-    }
+}
