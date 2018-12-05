d58c7bbc58447ac9b0c030f85232cc952d4f7a57
Izabela Kuzniar <iza8848@gmail.com>
2018-12-05 11:00:28 +0100

    pushing changes

--- src/js/Temp.ts
+++ src/js/Temp.ts
@@ -4,26 +4,26 @@ import axios, {
 } from "../../node_modules/axios";
 
 interface ITemp {
-  DT: Date;
+  dt: Date;
   id: number;
-  Temp: number;
+  temp: number;
 }
 
 google.charts.load('current', { packages: ['corechart', 'line'] });
 google.charts.setOnLoadCallback(tempDataChart);
 
 function tempDataChart(): void {
-  let uri: string = "https://restservicefrombroadcast20181120110311.azurewebsites.net/api/dts";
+  let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/Temperature";
 
   axios.get<ITemp>(uri)
     .then(function (response: AxiosResponse<ITemp[]>): void {
       console.log(response);
       var data = new google.visualization.DataTable();
-      data.addColumn('string', 'X');
-      data.addColumn('number', 'Temp');
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'temp');
 
       response.data.sort((a: ITemp, b: ITemp) => {
-        return new Date(Date.parse(a.DT.toString())).getTime() - new Date(Date.parse(b.DT.toString())).getTime();
+        return new Date(Date.parse(a.dt.toString())).getTime() - new Date(Date.parse(b.dt.toString())).getTime();
       });
 
       var newarr = [response.data[0]];
@@ -35,7 +35,7 @@ function tempDataChart(): void {
 
       newarr.forEach((temp: ITemp) => {
         data.addRows([
-            [temp.DT.toString().split('T')[0], temp.Temp]
+            [temp.dt.toString().split('T')[0], temp.temp]
           ]);
         
           var options = {
@@ -77,3 +77,23 @@ function tempDataChart(): void {
     }); 
     })
 }
+
+let outPut: HTMLOutputElement = <HTMLOutputElement> document.getElementById("outPut");
+
+let uri: string = "https://thebertharestconsumer20181031102055.azurewebsites.net/api/Temperature";
+
+
+axios.get<ITemp>(uri)
+           .then(function(response:AxiosResponse<ITemp[]>): void {
+              let result: string = "<table><tr><th>Id</th><th>Temperature</th><th>Date</th>" 
+                response.data.forEach((temp: ITemp) => {
+              result += "<tr><td>" + temp.id + "</td><td>" + temp.temp + "</td><td>" + temp.dt + "</td>";
+            });
+        result += "</table>"
+        outPut.innerHTML = result;
+        })
+        .catch (function (error: AxiosError): void {
+           if (error.response) {
+               outPut.innerHTML = error;}
+           else {outPut.innerHTML = error;}
+        })
